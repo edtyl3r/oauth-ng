7537006217240ab2cdc1b3f9671b36b5f5783199
Jeff Brannon <Jeff.Brannon@cm2000.co.uk>
2016-02-09 11:46:00 +0000

    Changed storage to cookies

--- app/scripts/services/storage.js
+++ app/scripts/services/storage.js
@@ -1,29 +1,26 @@
 'use strict';
 
-var storageService = angular.module('oauth.storage', ['LocalForageModule']);
+var storageService = angular.module('oauth.storage', ['ngCookies']);
 
-storageService.factory('Storage', ['$rootScope', '$localForage', function($rootScope, $localForage){
+storageService.factory('Storage', ['$rootScope', '$cookies', function ($rootScope, $cookies) {
 
-  var service = {
-  };
+    var service = {};
 
   /**
    * Deletes the item from storage,
    * Returns the item's previous value
    */
   service.delete = function (name) {
-    var stored = this.get(name);
-    $localForage.removeItem(name)
-    return stored;
+      var stored = this.get(name);
+      $cookies.remove('oauthng-' + name);
+      return stored;
   };
 
   /**
    * Returns the item from storage
    */
   service.get = function (name) {
-    $localForage.getItem(name).then(function(data) {
-           return data;
-       });
+      return $cookies.getObject('oauthng-' + name);
   };
 
   /**
@@ -31,24 +28,14 @@ storageService.factory('Storage', ['$rootScope', '$localForage', function($rootS
    * Returns the item's value
    */
   service.set = function (name, value) {
-    $localForage.setItem(name, value).then(function() {
-      return $localForage.getItem(name);
-    });
+      $cookies.putObject('oauthng-' + name, value);
+      return this.get(name);
   };
 
   /**
    * Change the storage service being used
    */
-  service.use = function (storage) {
-    if (storage === 'indexDB') {
-      $localForage.setDriver($localForage.INDEXEDDB);
-    }
-    if (storage === 'localStorage') {
-      $localForage.setDriver($localForage.LOCALSTORAGE);
-    }
-    if (storage === 'webSQL') {
-      $localForage.setDriver($localForage.WEBSQL);
-    }
+  service.use = function () {
   };
 
   return service;
--- dist/oauth-ng.js
+++ dist/oauth-ng.js
@@ -1,4 +1,4 @@
-/* oauth-ng - v0.4.8 - 2016-02-09 */
+/* oauth-ng - v0.4.10 - 2016-02-09 */
 
 'use strict';
 
@@ -285,30 +285,27 @@ profileClient.factory('Profile', ['$http', 'AccessToken', '$rootScope', function
 
 'use strict';
 
-var storageService = angular.module('oauth.storage', ['LocalForageModule']);
+var storageService = angular.module('oauth.storage', ['ngCookies']);
 
-storageService.factory('Storage', ['$rootScope', '$localForage', function($rootScope, $localForage){
+storageService.factory('Storage', ['$rootScope', '$cookies', function ($rootScope, $cookies) {
 
-  var service = {
-  };
+    var service = {};
 
   /**
    * Deletes the item from storage,
    * Returns the item's previous value
    */
   service.delete = function (name) {
-    var stored = this.get(name);
-    $localForage.removeItem(name)
-    return stored;
+      var stored = this.get(name);
+      $cookies.remove('oauthng-' + name);
+      return stored;
   };
 
   /**
    * Returns the item from storage
    */
   service.get = function (name) {
-    $localForage.getItem(name).then(function(data) {
-           return data;
-       });
+      return $cookies.getObject('oauthng-' + name);
   };
 
   /**
@@ -316,29 +313,18 @@ storageService.factory('Storage', ['$rootScope', '$localForage', function($rootS
    * Returns the item's value
    */
   service.set = function (name, value) {
-    $localForage.setItem(name, value).then(function() {
-      return $localForage.getItem(name);
-    });
+      $cookies.putObject('oauthng-' + name, value);
+      return this.get(name);
   };
 
   /**
    * Change the storage service being used
    */
-  service.use = function (storage) {
-    if (storage === 'indexDB') {
-      $localForage.setDriver($localForage.INDEXEDDB);
-    }
-    if (storage === 'localStorage') {
-      $localForage.setDriver($localForage.LOCALSTORAGE);
-    }
-    if (storage === 'webSQL') {
-      $localForage.setDriver($localForage.WEBSQL);
-    }
+  service.use = function () {
   };
 
   return service;
 }]);
-
 'use strict';
 
 var oauthConfigurationService = angular.module('oauth.configuration', []);
